bb675652d09583d9c894cfaab3c44003ec29d0eb
Erin Kinney <erinckinney@gmail.com>
2021-10-23 19:59:06 -0700

    Add refinements

--- code/static/js/logic.js
+++ code/static/js/logic.js
@@ -6,77 +6,82 @@
 //   zoom: 14
 // });
 
-function loadHomepage(event=null)
-{
-  console.log(event);
+// function loadHomepage(event=null)
+// {
+//   console.log(event);
 
-  let dropdownMenu = document.getElementById("selDataset");
-  let shortLocation;
-  if (dropdownMenu == null)  {
-    shortLocation = "UCLA";
-  }
-  else
-  {
-    shortLocation = dropdownMenu.options[dropdownMenu.options.selectedIndex].value;
-  }
+//   let dropdownMenu = document.getElementById("selDataset");
+//   let shortLocation;
+//   if (dropdownMenu == null)  {
+//     shortLocation = "UCLA";
+//   }
+//   else
+//   {
+//     shortLocation = dropdownMenu.options[dropdownMenu.options.selectedIndex].value;
+//   }
 
-  d3.json(`/data2/${shortLocation}`).then(function(data){
+d3.json("/data2").then(function(data){
 
-    console.log(data["locations"]);
-
-    let campusLocations = data["locations"];
-    var dropdownOptions = [];
-    for (let i = 0; i < campusLocations.length; i++) {
-      let campus = campusLocations[i];
-      let campusName = campus["locationName"];
-      let shortName = campus["shortName"];
+  // Set values for dropdown menu
+  let campusLocations = data["locations"];
+  var dropdownOptions = [];
+  for (let i = 0; i < campusLocations.length; i++) {
+    let campus = campusLocations[i];
+    let campusName = campus["locationName"];
+    let shortName = campus["shortName"];
       
-      dropdownOptions.push({"shortName": shortName, "campusName": campusName});
-    }
-    // dropdownOptions = data["locations"].map(data => {"shortName": data["shortName"], "locationName": data["locationName"]});
+    dropdownOptions.push({"shortName": shortName, "campusName": campusName});
+  }
 
-    // Use D3 to select dropdown menu
-    var dropdown = d3.select("#selDataset");
+  // Use D3 to select dropdown menu
+  var dropdown = d3.select("#selDataset");
 
-    // Append dropdown options to menu
-    dropdownOptions.forEach(option => {
-      dropdown.append("option").text(option["campusName"]).property("value", option["shortName"]);
-    });
+  // Append dropdown options to menu
+  dropdownOptions.forEach(option => {
+    dropdown.append("option").text(option["campusName"]).attr("value", option["shortName"]).attr("name", option["shortName"]);
+  });
 
-    // Leaflet map
-    var myMap = L.map('map').setView([34.0689, -118.4452], 15);
+// do not use data anymore
+});
 
+d3.json("/data2").then(function(locationData){
 
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 18,
-      id: 'mapbox/light-v10',
-      tileSize: 512,
-      zoomOffset: -1,
-      accessToken: 'pk.eyJ1IjoibmFwcHl0cmFpbHMiLCJhIjoiY2t1dXBvYXJ1MWlhNzJ1cGphZXJ3MHJ4diJ9.r-J1-mrEKBLhc2V3Kw5wXA'
-    }).addTo(myMap);
+  // Leaflet map
+  let lat = parseFloat(locationData["locations"][0]["latitude"])
+  let lon = parseFloat(locationData["locations"][0]["longitude"])
+  console.log(`${lat}, ${lon}`)
+
+  var myMap = L.map('map').setView([lat, lon], 15);
 
 
-    // Then move the map
-    myMap.panBy(new L.Point(0, -75), {animate: false});
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/light-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibmFwcHl0cmFpbHMiLCJhIjoiY2t1dXBvYXJ1MWlhNzJ1cGphZXJ3MHJ4diJ9.r-J1-mrEKBLhc2V3Kw5wXA'
+  }).addTo(myMap);
 
-    // Creating a new marker:
-    // We pass in some initial options, and then add the marker to the map by using the addTo() method.
-    var campus = L.circle([34.0689, -118.4452], {
-      color: "#70DB70",
-      weight: 15,
-      stroke: true,
-      fillColor: "yellowgreen",
-      fillOpacity: 0.5,
-      radius: 550
-    }).addTo(myMap);
 
-    // Binding a popup to our marker
-    campus.bindPopup("<center><b>Current<br>Conditions</b><br><img src='https://api.weather.gov/icons/land/day/skc?size=medium'><br><hr><b>Sunny</b><br>Temperature: 75° F<br>Wind Speed: 5mph<br>Wind Direction: NNW</center>");
-    // campus.bindPopup("<img src='https://api.weather.gov/icons/land/day/skc?size=medium'><br><hr><b>Sunny</b><br>Temperature: 75° F<br>Wind Speed: 5mph<br>Wind Direction: NNW").openPopup();
+  // Then move the map
+  //myMap.panBy(new L.Point(0, -75), {animate: false});
 
-    // Weekly forecast
-  let dailyForecasts = data["dailyForecasts"];
+  // Creating a new marker:
+  var campus = L.circle([34.0689, -118.4452], {
+    color: "#70DB70",
+    weight: 15,
+    stroke: true,
+    fillColor: "yellowgreen",
+    fillOpacity: 0.5,
+    radius: 550
+  }).addTo(myMap);
+
+  // Binding a popup to the circle
+  campus.bindPopup("<center><b>Current<br>Conditions</b><br><img src='https://api.weather.gov/icons/land/day/skc?size=medium'><br><hr><b>Sunny</b><br>Temperature: 75° F<br>Wind Speed: 5mph<br>Wind Direction: NNW</center>");
+
+  // Weekly forecast
+  let dailyForecasts = locationData["dailyForecasts"];
   var forecastPeriods = [];
   for (let i = 0; i < dailyForecasts.length; i++) {
     let period = dailyForecasts[i];
@@ -104,23 +109,23 @@ function loadHomepage(event=null)
     auxp.append("div").text(forecastPeriods[i]["periodShortForecast"])
     auxp.append("div").text(forecastPeriods[i]["periodTemperature"] + "° F");
     auxp.append("div").text(forecastPeriods[i]["periodDetailedForecast"])
-    auxp.append("hr").attr("style", "")
+    auxp.append("hr").attr("class", "weather-divider")
     } 
 
-  // do not use data anymore
-  });
-}
+// do not use localData anymore
+});
+
+// }
 
 
-=======
-loadDropdown();
-loadHomepage();
+// loadDropdown();
+// loadHomepage();
 
-// let dropdownMenu = document.getElementById("selDataset")
-// dropdownMenu.addEventListener("change", loadHomepage);
+// // let dropdownMenu = document.getElementById("selDataset")
+// // dropdownMenu.addEventListener("change", loadHomepage);
 
-document.getElementById("selDataset").addEventListener("change", function(event){
-  // console.log(event)
-  console.log(dropdownMenu.options[dropdownMenu.options.selectedIndex].value)
-})
+// document.getElementById("selDataset").addEventListener("change", function(event){
+//   // console.log(event)
+//   console.log(dropdownMenu.options[dropdownMenu.options.selectedIndex].value)
+// })
 
